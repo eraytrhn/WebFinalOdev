bd7b0d583a437d61cae3c8bfa329a3457ca8cdf8
ercha <vxc67973@gmail.com>
2022-01-20 01:23:37 +0300

    Başlangıç

---
+++ WebFinalOdev/Controllers/HobiUrunuController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFinalOdev.Controllers
+{
+    public class HobiUrunuController : Controller
+    {
+        // GET: HobiUrunleri
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HobiUrunleri/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HobiUrunleri/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HobiUrunleri/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HobiUrunleri/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HobiUrunleri/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HobiUrunleri/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HobiUrunleri/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
